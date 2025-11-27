eb1e1dd598e5bf0d275ad1865c07dbd0a1702a13
SURENDRA BABU <surendrasvsk@gmail.com>
2025-11-27 15:18:25 +0530

    Fix clientPhoto migration, update models, finalize user registration API

--- users_app/models.py
+++ users_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.contrib.auth.hashers import make_password, check_password
 
 # ---------- Custom User Manager ----------
 class UserRegManager(BaseUserManager):
@@ -9,18 +8,11 @@ class UserRegManager(BaseUserManager):
             raise ValueError("Email is required")
         email = self.normalize_email(email)
 
-        # Ensure phone is stored as None (not empty string) when not provided
-        phone = extra_fields.pop('userPhone', None) or None
-
-        # Create user instance with phone explicitly set
-        user = self.model(email=email, userName=userName, userPhone=phone, **extra_fields)
-
-        # Set password or make unusable password if none provided
+        user = self.model(email=email, userName=userName, **extra_fields)
         if password:
             user.set_password(password)
         else:
             user.set_unusable_password()
-
         user.save(using=self._db)
         return user
 
@@ -28,12 +20,6 @@ class UserRegManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
         return self.create_user(email, userName, password, **extra_fields)
 
 
@@ -42,7 +28,6 @@ class userReg(AbstractBaseUser, PermissionsMixin):
     userId = models.AutoField(primary_key=True)
     userName = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    # allow NULL/blank and keep uniqueness for non-null values
     userPhone = models.CharField(max_length=10, unique=True, null=True, blank=True)
     userLocation = models.CharField(max_length=100, default="yourcity")
 
@@ -62,19 +47,19 @@ class userReg(AbstractBaseUser, PermissionsMixin):
 class clientReg(models.Model):
     clientName = models.CharField(max_length=100)
     clientEmail = models.EmailField(unique=True)
-    # allow NULL/blank for client phone as well
     clientPhone = models.CharField(max_length=10, null=True, blank=True)
     clientLocation = models.CharField(max_length=50, default="yourcity")
     clientPhoto = models.CharField(max_length=500)
     clientPassword = models.CharField(max_length=255)
 
     def save(self, *args, **kwargs):
-        # Hash password only on create (when pk not present)
+        # Hash password only on create
         if not self.pk:
             self.clientPassword = make_password(self.clientPassword)
         super().save(*args, **kwargs)
 
     def check_client_password(self, raw_password):
+        from django.contrib.auth.hashers import check_password
         return check_password(raw_password, self.clientPassword)
 
     def __str__(self):
--- users_app/serializers.py
+++ users_app/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
 from .models import userReg, clientReg
 
 class UserRegSerializer(serializers.ModelSerializer):
@@ -8,33 +7,37 @@ class UserRegSerializer(serializers.ModelSerializer):
         fields = ("userId","userName","email","userPhone","userLocation","password")
         read_only_fields = ("userId",)
         extra_kwargs = {
-            "password": {"write_only": True}  # write only
+            "password": {"write_only": True}
         }
 
     def create(self, validated_data):
-        pwd = validated_data.get("password")
-        if pwd:
-            validated_data["password"] = make_password(pwd)
-        return super().create(validated_data)
+        password = validated_data.pop('password', None)
+        user = userReg(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
 
     def update(self, instance, validated_data):
-        if "password" in validated_data:
-            pwd = validated_data.get("password")
-            if pwd:
-                validated_data["password"] = make_password(pwd)
-        return super().update(instance, validated_data)
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 class ClientRegSerializer(serializers.ModelSerializer):
     class Meta:
         model = clientReg
         fields = ("clientName","clientEmail","clientPhone","clientLocation","clientPassword")
-        extra_kwargs = {
-            "clientPassword": {"write_only": True}
-        }
+        extra_kwargs = {"clientPassword": {"write_only": True}}
 
     def create(self, validated_data):
-        pwd = validated_data.get("clientPassword")
-        if pwd:
-            validated_data["clientPassword"] = make_password(pwd)
-        return super().create(validated_data)
+        password = validated_data.pop("clientPassword", None)
+        client = clientReg(**validated_data)
+        if password:
+            client.clientPassword = make_password(password)
+        client.save()
+        return client
