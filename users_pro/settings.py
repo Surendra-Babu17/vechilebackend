ce30d6b45d38358c7bf715568b179c4937720003
SURENDRA BABU <surendrasvsk@gmail.com>
2025-11-28 12:53:27 +0530

    Prepare for Render deployment: update settings and env

--- users_pro/settings.py
+++ users_pro/settings.py
@@ -102,15 +102,16 @@ pymysql.install_as_MySQLdb()
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
         "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT", default="3306"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "PORT": env("DB_PORT", default="5432"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
+
 
 # ------------------------------
 # PASSWORD VALIDATION
